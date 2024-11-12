ef6b7bd2a95b229c39a784901841f63dc43e25de
Milad Golfam <milad.golfam@gmail.com>
2024-11-12 11:31:17 +0330

    Fix: brotli deprecated from this library

--- utils/compression/br.go
+++ utils/compression/br.go
@@ -3,32 +3,27 @@
 
 package utils
 
-import (
-	"bytes"
-	"io"
+// "github.com/google/brotli/go/cbrotli"
 
-	"github.com/google/brotli/go/cbrotli"
-)
+// func BrCompress(data []byte) ([]byte, error) {
+// 	var buf bytes.Buffer
+// 	writer := cbrotli.NewWriter(&buf, cbrotli.WriterOptions{})
+// 	_, err := writer.Write(data)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	err = writer.Close()
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return buf.Bytes(), nil
+// }
 
-func BrCompress(data []byte) ([]byte, error) {
-	var buf bytes.Buffer
-	writer := cbrotli.NewWriter(&buf, cbrotli.WriterOptions{})
-	_, err := writer.Write(data)
-	if err != nil {
-		return nil, err
-	}
-	err = writer.Close()
-	if err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
-}
-
-func BrDecompress(data []byte) ([]byte, error) {
-	reader := cbrotli.NewReader(bytes.NewReader(data))
-	decompressed, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
-	return decompressed, nil
-}
+// func BrDecompress(data []byte) ([]byte, error) {
+// 	reader := cbrotli.NewReader(bytes.NewReader(data))
+// 	decompressed, err := io.ReadAll(reader)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return decompressed, nil
+// }
